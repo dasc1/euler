5db7b448b8177d02d6e807b7b07da96cbce9b90d
dasc1 <david1.scott+github@gmail.com>
2014-02-23 20:42:33 +0000

    problem 5

---
+++ problems/005.exs
@@ -0,0 +1,11 @@
+defmodule X do
+    
+    def ans do
+        a = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 # primes
+        b = a * 2 * 3 # prime squares
+        c = b * 2 # prime cubes
+        d = c * 2 # prime quads
+        d
+    end
+
+end
